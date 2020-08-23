edec0496fa7900a936f8e836e29e4d10aa866f2d
mafusuke <mafusukee@gmail.com>
2020-08-23 15:05:29 +0900

    36進数変換-絵文字、情報の配置工夫中

--- costume.py
+++ costume.py
@@ -14,13 +14,8 @@ class Costume(commands.Cog):
 
     def initialize_user_data(self, user_id: str):
         self.bot.database[user_id] = {
-                "current": 0,
-                "canvas": [
-                    {
-                        "name": "canvas1",
-                        "data": "10101010101"
-                    }
-                ]
+                "canvas": "1O4ZW5",
+                "save": []
             }
 
     async def cog_before_invoke(self, ctx):
@@ -40,7 +35,20 @@ class Costume(commands.Cog):
         base.paste(back, (0, 0), back)
         base.paste(weapon, (0, 0), weapon)
         base = self.convert_to_bytes(base)
-        await ctx.send(file=discord.File(fp=io.BytesIO(base), filename="result.png"))
+        canvas_name = self.bot.database[str(ctx.author.id)]["canvas"]
+        embed = discord.Embed()
+        item_id = parse_item_list_to_code([base_id, character_id, weapon_id, head_id, body_id, back_id])
+        text = f"{self.emoji['base'][str(base_id)]}{self.emoji['character'][str(character_id)]}{self.emoji['weapon'][str(weapon_id)]}{self.emoji['head'][str(head_id)]}{self.emoji['body'][str(body_id)]}{self.emoji['back'][str(back_id)]}"#f"装飾コード: {item_id}"
+        embed.add_field(name="ベース色", value=f"{base_id} {self.emoji['base'][str(base_id)]}")
+        embed.add_field(name="キャラクター", value=f"{character_id} {self.emoji['character'][str(character_id)]}")
+        embed.add_field(name="武器", value=f"{weapon_id} {self.emoji['weapon'][str(weapon_id)]}")
+        embed.add_field(name="頭装飾", value=f"{head_id} {self.emoji['head'][str(head_id)]}")
+        embed.add_field(name="体装飾", value=f"{body_id} {self.emoji['body'][str(body_id)]}")
+        embed.add_field(name="背中装飾", value=f"{back_id} {self.emoji['back'][str(back_id)]}")
+
+        #embed = discord.Embed(title=f"装飾コード: {item_id}\n{base_id} {self.emoji['base'][str(base_id)]}\n{character_id} {self.emoji['character'][str(character_id)]}\n{weapon_id} {self.emoji['weapon'][str(weapon_id)]}\n{head_id} {self.emoji['head'][str(head_id)]}\n{body_id} {self.emoji['body'][str(body_id)]}\n{back_id} {self.emoji['back'][str(back_id)]}")
+        #embed.set_author(name=f"装飾コード: {item_id}\n{base_id} {self.emoji['base'][str(base_id)]}\n{character_id} {self.emoji['character'][str(character_id)]}\n{weapon_id} {self.emoji['weapon'][str(weapon_id)]}\n{head_id} {self.emoji['head'][str(head_id)]}\n{body_id} {self.emoji['body'][str(body_id)]}\n{back_id} {self.emoji['back'][str(back_id)]}")
+        await ctx.send(text, embed=embed, file=discord.File(fp=io.BytesIO(base), filename="result.png"))
         return
 
     def convert_to_bytes(self, image: Image) -> bytes:
@@ -48,25 +56,24 @@ class Costume(commands.Cog):
         image.save(imgByteArr, format=image.format)
         return imgByteArr.getvalue()
 
-    def save_data_to_database(self, user_id, data: str) -> None:
-        current = self.bot.database[str(user_id)]["current"]
-        self.bot.database[str(user_id)]["canvas"][current]["data"] = data
+    def save_canvas_data(self, user_id, data: str) -> None:
+        self.bot.database[str(user_id)]["canvas"] = data
 
     @commands.command()
     async def set(self, ctx, *, item):
         item_list = item.split()
-        if len(item_list) == 1 and item.isdigit() and len(str(item)) == 11:
+        if len(item_list) == 1:
             code, result = check_item_id(item)
             if code == 0:
                 return await ctx.send(self.bot.error_text[result])
             await self.make_image(ctx, result[0], result[1], result[2], result[3], result[4], result[5])
-            self.save_data_to_database(ctx.author.id, parse_item_list_to_code(result))
+            self.save_canvas_data(ctx.author.id, parse_item_list_to_code(result))
         else:
             code, result = check_item_list(item_list)
             if code == 0:
                 return await ctx.send(self.bot.error_text[result])
             await self.make_image(ctx, result[0], result[1], result[2], result[3], result[4], result[5])
-            self.save_data_to_database(ctx.author.id, item)
+            self.save_canvas_data(ctx.author.id, parse_item_list_to_code(result))
 
     @commands.command()
     async def canvas(self, ctx):
@@ -75,14 +82,13 @@ class Costume(commands.Cog):
 
     @commands.command()
     async def show(self, ctx) -> None:
-        current = self.bot.database[str(ctx.author.id)]["current"]
-        item_code = self.bot.database[str(ctx.author.id)]["canvas"][current]["data"]
+        item_code = self.bot.database[str(ctx.author.id)]["canvas"]
         items = parse_item_code_to_list(item_code)
         await self.make_image(ctx, items[0], items[1], items[2], items[3], items[4], items[5])
-
-    # TODO: ランダムで作成する機能を追加
-
 
 
 def setup(bot):
     bot.add_cog(Costume(bot))
+
+
+# TODO: ランダムで作成する機能を追加
--- item_parser.py
+++ item_parser.py
@@ -1,8 +1,37 @@
-from typing import Any
+from typing import Any, List
+import string
+
+numbers = "0123456789"
+alphabets = string.ascii_letters
+characters = numbers + alphabets
+
+
+def parse_to_36(tmp: int):
+    result = ''
+    while tmp >= 36:
+        idx = tmp % 36
+        result = characters[idx] + result
+        tmp = int(tmp / 36)
+    idx = tmp % 36
+    return characters[idx] + result
+
+
+def parse_from_36(num: str) -> (int, Any):
+    """36進数→10進数に変換"""
+    try:
+        num = int(num, 36)
+    except:
+        return 0  # 不正な文字が含まれていた場合(0-9a-zA-Z以外)
+    else:
+        return 1, num
 
 
 def check_item_id(item: str) -> (int, Any):
     """ 装飾コードのリストの形式を確認して、装飾コードのリストを返す """
+    code, result = parse_from_36(item)  # 36進数から10進数に変換
+    if code == 0:
+        return 0, "wrong_costume_code"
+    item = str(result).zfill(11)  # 11桁になるように0埋めする
     if int(item[0:1]) not in [0, 1]:
         return 0, "wrong_base_id"
     if not 0 <= int(item[1:3]) <= 20:
@@ -18,7 +47,7 @@ def check_item_id(item: str) -> (int, Any):
     return 1, [int(item[0:1]), int(item[1:3]), int(item[3:5]), int(item[5:7]), int(item[7:9]), int(item[9:11])]
 
 
-def check_item_list(item: list) -> (int, Any):
+def check_item_list(item: List[str]) -> (int, Any):
     """ 装飾コードの形式を確認して、装飾コードのリストに変換 """
     if len(item) != 6:
         return 0, "missing_required_arguments"
@@ -37,9 +66,12 @@ def check_item_list(item: list) -> (int, Any):
     return 1, [int(item[0]), int(item[1]), int(item[2]), int(item[3]), int(item[4]), int(item[5])]
 
 
-def parse_item_list_to_code(item: list) -> str:
-    return f"{item[0]}{item[1]}{item[2]}{item[3]}{item[4]}{item[5]}"
+def parse_item_list_to_code(item: List[int]) -> str:
+    code = f"{item[0]}{str(item[1]).zfill(2)}{str(item[2]).zfill(2)}{str(item[3]).zfill(2)}{str(item[4]).zfill(2)}{str(item[5]).zfill(2)}"
+    return parse_to_36(int(code))
 
 
 def parse_item_code_to_list(item: str) -> list:
+    _, result = parse_from_36(item)  # 36進数から10進数に変換
+    item = str(result).zfill(11)  # 11桁になるように0埋めする
     return [int(item[0:1]), int(item[1:3]), int(item[3:5]), int(item[5:7]), int(item[7:9]), int(item[9:11])]
